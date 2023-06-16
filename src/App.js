a43dd1d79136259087b556822254c899010a811e
flouressaint <kadomtcev.va@students.dvfu.ru>
2023-06-16 14:38:21 +1000

    done table sort

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props) {
+  return <MyTable data={props.data} />;
+}
+
+function MyTable(props) {
+  const [data, setData] = useState(props.data);
+  let heads = [];
+  for (let keys in data[0]) {
+    heads.push(keys);
+  }
+  const radioCheck = heads.map((head, i) => (
+    <div className="radioElem">
+      <input
+        type="radio"
+        id={i}
+        name="radioCheck"
+        value={head}
+        onChange={handleClick}
+      ></input>
+      <label for={head}>{head}</label>
     </div>
+  ));
+  const head = heads.map((head) => <th>{head}</th>);
+  const dataTable = data.map((data) => (
+    <tr>
+      <td>{data[heads[0]]}</td>
+      <td>{data[heads[1]]}</td>
+      <td>{data[heads[2]]}</td>
+      <td>{data[heads[3]]}</td>
+      <td>{data[heads[4]]}</td>
+    </tr>
+  ));
+
+  function handleClick(event) {
+    let temp = [...data];
+    temp.sort(function (a, b) {
+      let result = 0;
+      let key = event.target.value;
+      if (a[key] < b[key]) {
+        result = -1;
+      } else if (a[key] > b[key]) {
+        result = 1;
+      }
+      return result;
+    });
+
+    setData(temp);
+  }
+
+  return (
+    <>
+      <div className="radioCheck">{radioCheck}</div>
+      <table>
+        <thead>
+          <tr>{head}</tr>
+        </thead>
+        <tbody>{dataTable}</tbody>
+      </table>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import yachts from "./yachts";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={yachts} />
+    <h1>ffff</h1>
   </React.StrictMode>
 );
 
